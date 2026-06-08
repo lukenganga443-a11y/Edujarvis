0133be1aa84036012072c59d095a236760f52781
anthony <lukenganga443@gmail.com>
2026-06-09 00:38:14 +0300

    Initial EduJarvis prototype

---
+++ script.js
@@ -0,0 +1,19 @@
+function askQuestion() {
+  document.getElementById("response").innerHTML =
+    "Photosynthesis is how plants use sunlight to make food.";
+}
+
+function explainSimple() {
+  document.getElementById("response").innerHTML =
+    "Imagine a plant cooking its lunch using sunlight.";
+}
+
+function examples() {
+  document.getElementById("response").innerHTML =
+    "Example: A sunflower uses sunlight to grow and make food.";
+}
+
+function quizMe() {
+  document.getElementById("response").innerHTML =
+    "Quiz: What does a plant need to perform photosynthesis?";
+}
